a1a849e599a12216ea806bb01b1b9038528181dd
Vincent Beraud <vincent.beraud@y-nov.com>
2014-11-03 09:17:06 +0100

    index

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->get('/', function () {
+    echo "Coucou!";
+});
+
+$app->get('/foo', function () {
+    echo "Foo!";
+});
+$app->run();
+?>

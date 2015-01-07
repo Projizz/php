b91abb2e2dfeab986c52aaf853f5cd4171214bf1
killianTrm <killian.terrom@y-nov.com>
2015-01-07 21:21:45 +0100

    partie projet

--- controleur/site/profil.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-
-// On affiche la page (vue)
-include_once('vue/site/profil.php');
-
--- vue/site/includes/menu.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-	<div class="col-md-2 col-md-offset-3">
-		<?php echo '<img class="img-circle img-responsive avatar" src="vue/site/assets/images/'.$_SESSION['id'].'.jpg" />';?>
-	</div>
-			<li ><a href="modele/site/deconnection.php">déconnexion</a></li>
-</div>
-    
